d09899b795ec9d7e215157f91ab585ed966eb5c5
jeffreyshen <jshen3@andrew.cmu.edu>
2025-02-08 03:47:12 -0500

    final day 1

--- src/Subsite.jsx
+++ src/Subsite.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Header from './Header';
 
 const SubSite = () => {
   const { subsiteId } = useParams(); // Get the subsiteId from the URL
 
   return (
     <div>
+        <Header />
       <h1>Welcome to {subsiteId} Subsite!</h1>
       {/* You can display other content related to this subsite */}
       <p>Details about {subsiteId}...</p>
